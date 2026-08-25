62d3faea36fd47800938d50c67b5a4313e9d2acd
Pallab Sil <silpallab1204@gmail.com>
2026-08-25 21:18:20 +0530

    Fix production API configuration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
 const studentRoutes = require("./routes/studentRoutes");
+const connectDB = require("./config/db");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/students", studentRoutes);
 app.use("/api/auth", authRoutes);
 
@@ -27,6 +29,6 @@ app.get("/", (req, res) => {
 // Server port
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
